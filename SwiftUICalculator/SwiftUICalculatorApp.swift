66d24bf3502a95a68d5b8879254cb423f6393bb6
Khondakar Afridi <afridi@tikweb.com>
2023-05-25 16:08:00 +0600

    Initial Commit

---
+++ SwiftUICalculator/SwiftUICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICalculatorApp.swift
+//  SwiftUICalculator
+//
+//  Created by Khondakar Afridi on 25/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
